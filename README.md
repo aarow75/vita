b3b5104accb498ca5fd312352a796205cf0a7dcc
Aaron Swensen <aaron.swensen@us.usana.com>
2019-07-10 13:35:41 -0600

    improved highlighting fixed formatting added more documentation

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # The Synoptic "Life of Adam and Eve"
 With comparisons to the Latin, Greek, Armenian, Georgian, and Slavonic. Also including cross references to the 1 Book of Enoch, Book of Jubilees, and LDS Book of Abraham and Book of Moses. Other comparisons with the Syriac Cave of Treasures, Koran, and various targums and midrash.
+
+`vita.json` contains the manuscript version, title, year of translation, and number of manuscripts the translation is based from. This provides the column header information
+
+`pericope.json` gives an `id`, title and list of manuscript sources that exist for that pericope. This is a way to topically group the related content. Potentially, it will include links to additional information about that pericope where the topic is expanding in other manuscripts (i.e. Fall of Satan manuscripts that fall outside of these primary manuscripts, or references to Ugarit, Book of Jubilees, or other sources that are related). The pericope id is generally numbered in a chronological order, which mostly but is not necessarily the order found in the manuscripts themselves.
+
+`greek.json`, `armenian.json`, `georgian.json`, `latin.json`, `slavonic.json` contain the content of the manuscripts, divided by pericope id, which stated above, is not necessarily the order found in the manuscript. Each `content` in the pericope group is in it's natural manuscript order, with the scholars numbering in bold.
+
+`bible.json` is directly related to the above json files, grouped by pericope, showing the related Genesis verses.
+
+`references.json` is also related to the above json files, including `bible.json` that include other document references (Book of Enoch, New Testament, Old Testament, etc.) that are related.
--- app.js
+++ app.js
@@ -14,9 +14,20 @@ function loadJSON(callback, path) {
 function getSource(name, abbr) {
     loadJSON(function(json) {
         // loop through pericope id's that match what the results are and put them there.
+        /*
+        The "id" is to group pericopes by common themes
+        the number inside <b></b> is tied to the manuscripts ordering and numbering.
+        This is a numbering that scholars have determined and doesn't always line up 
+        in different manuscripts particularly the Greek and Slavonic. The Latin, Georgian, 
+        and Armenian mostly line up.
+        TODO: I'd like switch this to using a atomic id for each manuscript pericope that can connect 
+        all lines in the pericopes to related content (including Genesis references). I want to preserve 
+        the pericope numbering but make it hideable and make the highlighting based on the atomic numbering.
+        */
         for (let i = 0; i < json.length; i++) {
             let id = json[i]["id"];
-            let content = Array.isArray(json[i]["content"]) ? json[i]["content"].join(" ") : json[i]["content"];
+            let regex = /\<b\>(.?\d\..?\d.?[a-z]{0,1})\<\/b\>/; // we will grab the second index
+            let content = Array.isArray(json[i]["content"]) ? json[i]["content"].map(f => f.match(regex) ? "<span line-id='" + f.match(regex)[1] + "'>" + f + "</span>" : f).join(" ") : json[i]["content"];
             document.querySelector(`.pericope[pericope-id='${id}'] *[data-column='${abbr}']`).innerHTML = content;
         }
     }, './assets/' + name + '.json');
@@ -40,6 +51,22 @@ function showAll() {
     document.querySelectorAll("[data-column]").forEach(f => f.classList.remove("hide"));
 }
 
+function lineIdMouseover(target) {
+    console.log("lineIdMouseover",target)
+    document.querySelectorAll(`[line-id='${target.getAttribute("line-id")}']`).forEach(f => f.classList.toggle("highlight"));
+}
+
+function lineIdMouseout(target) {
+    console.log("lineIdMouseover",target)
+    document.querySelectorAll(`[line-id='${target.getAttribute("line-id")}']`).forEach(f => f.classList.remove("highlight"));
+}
+
+function mouseEvents() {
+    document.querySelectorAll("[line-id]").forEach(f => f.addEventListener("mouseover", e => lineIdMouseover(e.target)));
+    document.querySelectorAll("[line-id]").forEach(f => f.addEventListener("mouseout", e => lineIdMouseout(e.target)));
+}
+
+// loads page and column titles and info
 loadJSON(function(json) {
     document.querySelector('#title').innerHTML = json.title;
     columnText("GEO", json);
@@ -53,13 +80,14 @@ loadJSON(function(json) {
 
 
 loadJSON(function(json) {
+    // creates the pericope headings
     for (let i = 0; i < json.pericopes.length; i++) {
         let pericope = `
       <tr><th colspan="6" class="title">[${json.pericopes[i]["id"]}] ${json.pericopes[i]["title"]}</th></tr>
       <tr class="pericope" pericope-id="${json.pericopes[i]["id"]}">
-        <td data-column="LAT" class="source"></td>
         <td data-column="GRE" class="source"></td>
         <td data-column="SLA" class="source"></td>
+        <td data-column="LAT" class="source"></td>
         <td data-column="GEO" class="source"></td>
         <td data-column="ARM" class="source"></td>
         <td data-column="BIB" class="source"></td>
@@ -70,6 +98,7 @@ loadJSON(function(json) {
         document.querySelector('.pericopes tbody').insertAdjacentHTML('beforeend', pericope);
     }
 
+    // loads the manuscript's JSON file into the appropriate columns under the proper pericope section
     getSource("latin", "LAT");
     getSource("armenian", "ARM");
     getSource("georgian", "GEO");
@@ -78,3 +107,5 @@ loadJSON(function(json) {
     getSource("bible", "BIB");
     getSource("references", "REF");
 }, './assets/pericope.json');
+
+setTimeout(mouseEvents, 2000);
